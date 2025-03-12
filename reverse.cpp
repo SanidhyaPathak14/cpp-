ab30cf8ee460d9260d8bad74a42759d3c0d5a878
sanidhya pathak <pathaksanidhya14@gmail.com>
2025-03-12 22:57:32 +0530

    Add files via upload

---
+++ reverse.cpp
@@ -0,0 +1,33 @@
+// program number(30)
+// reverse the number using while loop
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num, reversedNum = 0, remainder;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    while (num != 0)
+    {
+        remainder = num % 10;
+        reversedNum = reversedNum * 10 + remainder;
+        num /= 10;
+    }
+
+    cout << "Reversed number: " << reversedNum << endl;
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
